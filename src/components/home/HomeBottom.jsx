5b8001881c80e3739f37840db15a8c59b7aedf7e
fahimnirjon <fahimnirjon21@gmail.com>
2026-01-27 18:41:44 +0600

    features added

--- src/components/home/HomeBottom.jsx
+++ src/components/home/HomeBottom.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottom = () => {
   return (
-    <div>
-      Home Bottom
+    <div className='font-[font2] flex items-center justify-center gap-3'>
+      <Link className='text-[6.5vw] uppercase border-2
+       border-white rounded-full px-6 pt-2 leading-[6vw] '>Projects</Link>
+      <Link className='text-[6.5vw] uppercase border-2
+       border-white rounded-full px-6 pt-2 leading-[6vw] '>Agency</Link>
     </div>
   )
 }
--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Video from "./Video";
 
 const HomeHero = () => {
   return (
-    <div>
-      Home Hero
+    <div className="font-[font1] pt-5 text-center">
+      <div className="uppercase text-[9vw] leading-[8vw] flex items-center justify-center">L'étincelle</div>
+      <div className="uppercase text-[9vw] leading-[8vw] flex items-center justify-center">
+        qui <div className="h-[7vw] rounded-full overflow-hidden mx-2 -mt-5 w-[14vw]">
+            <Video/>
+            </div> génère
+      </div>
+      <div className="uppercase text-[9vw] leading-[8vw] flex items-center justify-center">la créativité</div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeHero
+export default HomeHero;
